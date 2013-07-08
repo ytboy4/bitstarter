486dcada723dfa4506fe06e9752fa7f80c9f6a13
Ubuntu <ubuntu@ip-172-31-14-113.eu-west-1.compute.internal>
2013-07-08 10:04:55 +0000

    Updated web.js with fs.ReadFileSync()

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buffer = new Buffer("index.html");
+var out = buffer.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
