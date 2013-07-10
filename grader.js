ae35e23c4f7dfda3a9423d4bb176eedc866a89d3
Ubuntu <ubuntu@ip-172-31-14-113.eu-west-1.compute.internal>
2013-07-10 07:32:04 +0000

    Grader accepts url but returns wrong results

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -45,7 +46,7 @@ var loadChecks = function(checksfile) {
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-  $ = cheerioHtmlFile(htmlfile);
+  $ = htmlfile;
   var checks = loadChecks(checksfile).sort();
   var out = {};
   for(var ii in checks) {
@@ -65,10 +66,23 @@ if(require.main == module) {
   program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <html_file>', 'URL to index.html')
     .parse(process.argv);
-  var checkJson = checkHtmlFile(program.file, program.checks);
-  var outJson = JSON.stringify(checkJson, null, 4);
-  console.log(outJson);
+    if(program.url){
+      rest.get(program.url).on('complete', function(result){
+        $ = cheerio.load(fs.readFile(result, function(err,data){
+         var checkJson = checkHtmlFile($, program.checks);
+         var outJson = JSON.stringify(checkJson, null, 4);
+         console.log(outJson);
+        }));
+      });
+    }
+    else {
+      $ = cheerio.load(fs.readFileSync(program.file));
+      var checkJson = checkHtmlFile($, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
 } else {
   exports.checkHtmlFile = checkHtmlFile;
 }
